dad2ceccd99a886ed844c8a956b15b747422a644
abhishekdahiya-db <abhishekdahiya.dahiya48@gmail.com>
2024-03-08 14:12:58 +0530

    skeleton code for micro-services

---
+++ src/main/java/com/wallet/paytmwallet/PaytmEWalletApplication.java
@@ -0,0 +1,11 @@
+package com.wallet.paytmwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmEWalletApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmEWalletApplication.class, args);
+	}
+}
